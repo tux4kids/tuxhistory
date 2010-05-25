3bf68a001830159904b13a298ce03ceae69c4c27
julio <julio@julio-desktop.(none)>
2010-05-25 12:51:53 -0500

    Copy of TuxMath

---
+++ config.h.cmake
@@ -0,0 +1,17 @@
+#cmakedefine HAVE_ERROR_H 1
+#cmakedefine HAVE_SCANDIR 1
+
+#cmakedefine HAVE_GETTEXT 1
+#cmakedefine ENABLE_NLS 1
+#cmakedefine SDL_Pango 1
+#cmakedefine HAVE_RSVG 1
+
+/* Stuff needed for linewrap */
+#cmakedefine LINEBREAK 1
+#cmakedefine HAVE_STDINT_H 1
+#cmakedefine ICONV_SECOND_ARGUMENT_IS_CONST 1
+#ifdef ICONV_SECOND_ARGUMENT_IS_CONST
+#define ICONV_CONST const
+#else
+#define ICONV_CONST
+#endif
